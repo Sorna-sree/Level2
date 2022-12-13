b0c4d13516a03eabc570048f6c07c9fdc5469154
Sorna-sree <110393066+Sorna-sree@users.noreply.github.com>
2022-12-13 12:07:29 +0530

    Add files via upload

--- level2 ex2 string manipulation2.py
+++ level2 ex2 string manipulation2.py
@@ -3,8 +3,22 @@
 #If same breakfast item is mentioned, print it only once.
 
 breakfast_list=[]
+uniqueList=[]
+duplicateList=[]
+
 for index in range(0,5):
-    breakfast=input("what he likes for breakfast: ")
+    breakfast=input("what do you like for breakfast  ")
     breakfast_list.append(breakfast)
     
-print(f'user likes {set(breakfast_list)} for breakfast')
+#print(f'user likes {set(breakfast_list)} for breakfast')
+
+#find the same breakfast items and print only once
+for food in breakfast_list:
+    if food not in uniqueList:
+        uniqueList.append(food)
+    elif food not in duplicateList:
+        duplicateList.append(food)
+
+print("breakfast item is: ",uniqueList)
+
+                
